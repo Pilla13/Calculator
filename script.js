78529e09033c18c0ee7cf1d420f08056fadc26d0
pilla13 <PolliSims@yandex.ru>
2023-10-14 22:31:14 +0300

    calc

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const { func } = require("prop-types");
+
 let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
@@ -41,10 +43,48 @@ function handleSymbol(symbol) {
       break;
 
     case "−":
-    case "÷":
+    case "+":
     case "×":
     case "÷":
       handleMath(symbol);
       break;
   }
 }
+
+function handleMath(symbol) {
+    if(buffer === '0'){
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if(runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperator(intBuffer);
+    }
+
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperator(intBuffer) {
+    if(previousOperator === '+'){
+        runningTotal += intBuffer;
+    } else if(previousOperator === '−'){
+        runningTotal -= intBuffer;
+    } else if(previousOperator === '×'){
+        runningTotal *= intBuffer;
+    } else if(previousOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber(numberString) {
+    if(buffer === "0"){
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+}
+
