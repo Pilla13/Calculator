716562e14f39a573617269c386388940b929c4d3
pilla13 <PolliSims@yandex.ru>
2023-10-14 22:40:25 +0300

    ready

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const { func } = require("prop-types");
-
 let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
@@ -38,7 +36,7 @@ function handleSymbol(symbol) {
       if (buffer.length === 1) {
         buffer = "0";
       } else {
-        buffer = buffer.toString(0, buffer.length - 1);
+        buffer = buffer.substring(0, buffer.length - 1);
       }
       break;
 
@@ -52,39 +50,46 @@ function handleSymbol(symbol) {
 }
 
 function handleMath(symbol) {
-    if(buffer === '0'){
-        return;
-    }
+  if (buffer === "0") {
+    return;
+  }
 
-    const intBuffer = parseInt(buffer);
+  const intBuffer = parseInt(buffer);
 
-    if(runningTotal === 0) {
-        runningTotal = intBuffer;
-    } else {
-        flushOperator(intBuffer);
-    }
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperator(intBuffer);
+  }
 
-    previousOperator = symbol;
-    buffer = '0';
+  previousOperator = symbol;
+  buffer = "0";
 }
 
 function flushOperator(intBuffer) {
-    if(previousOperator === '+'){
-        runningTotal += intBuffer;
-    } else if(previousOperator === '−'){
-        runningTotal -= intBuffer;
-    } else if(previousOperator === '×'){
-        runningTotal *= intBuffer;
-    } else if(previousOperator === '÷'){
-        runningTotal /= intBuffer;
-    }
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "−") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === "÷") {
+    runningTotal /= intBuffer;
+  }
 }
 
 function handleNumber(numberString) {
-    if(buffer === "0"){
-        buffer = numberString;
-    } else {
-        buffer += numberString;
-    }
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
 }
 
+function init() {
+  document.querySelector(".calc-buttons").addEventListener("click", (e) => {
+    buttonClick(e.target.innerText);
+  });
+}
+
+init();
